a8a258ab19d8eeb98d9637d1c4068ec58c9201bd
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-06-24 15:34:33 +0100

    feat: added 'Component.Explorer' settings explicitly

--- quartz/quartz.layout.ts
+++ quartz/quartz.layout.ts
@@ -38,7 +38,17 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      title: "Explorer", // title of the explorer component
+      folderClickBehavior: "link", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
+      folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
+      useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
+      // sortFn: ...,
+      // filterFn: ...,
+      // mapFn: ...,
+      // what order to apply functions in
+      order: ["filter", "map", "sort"],
+    })
   ],
   right: [
     Component.Graph(),
